609ede6fd140861e7d712376565c1b74e43c5b5d
Smit <145379300+Smit1403@users.noreply.github.com>
2024-01-07 16:26:20 -0700

    further changes to chat.py The AI can now perfectly tell time and will catch errors and ask for another prompt.

--- mypythonAI/chat.py
+++ mypythonAI/chat.py
@@ -1,10 +1,12 @@
 # Smit saraiya
 
+import time
 import numpy as np
 import speech_recognition as sr
 from gtts import gTTS
 import os
 import datetime
+import transformers
 
 class ChatBot():
     def __init__(self, name):
@@ -16,6 +18,7 @@ class ChatBot():
         with sr.Microphone() as source:
             print("Listening....")
             audio = recognizer.listen(source)
+            self.text = "ERROR"
             # some problem over here
         try:
             self.text = recognizer.recognize_google(audio)
@@ -35,27 +38,48 @@ class ChatBot():
         print("AI ---> ", text)
         speaker = gTTS(text=text, lang="en", slow=False)
         speaker.save("response.mp3")
+        statbuf = os.stat("response.mp3")
+        mbytes = statbuf.st_size / 1024
+        duration = mbytes / 2000
         os.system("afplay response.mp3")
+        time.sleep(int(50*duration))
         os.remove("response.mp3")
         
         
         
 if  __name__ == "__main__":
     bot = ChatBot(name="computer")
-    
-    while True:
+    nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+    ex = True
+    while ex:
         bot.speech_to_text()
         # wakeup
         if bot.wake_up(bot.text) is True:
             res = "Hello, my name is computer. What can I do for you?"
         elif "time" in bot.text:
-            res = bot.action_time()
-        elif any(i in bot.text for i in ["thanks", "thanks"]):
+            res = "The time is " + str(bot.action_time())
+        elif any(i in bot.text for i in ["thank", "thanks"]):
             res = np.random.choice(
                 ["You're Welcome!",
                  "Anytime!",
                  "No problem!",
                  "Always here to help!"])
-                        
+        elif any(i in bot.text for i in ["exit", "close", "bye"]):
+            res = np.random.choice(
+                ["See you soon!",
+                 "Bye!",
+                 "Goodbye!",
+                 "Ciao!"])
+            ex = False
+        else:
+            if bot.text == "ERROR":
+                res = "Sorry, I didn't get that."
+            else:
+                chat = nlp(transformers.Conversation(bot.text), pad_token_id=50256)
+                res = str(chat)
+                res = res[res.find("bot >> ")+6:].strip()
         bot.text_to_speech(res)
+        
+    print("-- shutting down --")
 
