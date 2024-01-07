5d0df906652d08f02dd4de901b51730b93389047
Smit <145379300+Smit1403@users.noreply.github.com>
2024-01-07 15:02:32 -0700

    slight working solution

--- mypythonAI/chat.py
+++ mypythonAI/chat.py
@@ -4,6 +4,7 @@ import numpy as np
 import speech_recognition as sr
 from gtts import gTTS
 import os
+import datetime
 
 class ChatBot():
     def __init__(self, name):
@@ -19,15 +20,18 @@ class ChatBot():
         try:
             self.text = recognizer.recognize_google(audio)
             print("me ---> ", self.text)
-        except:
-            print("me ---> ERROR")
-    
+        except Exception as e:
+            print("me ---> ERROR", str(e))
     
     def wake_up(self, text):
         return True if self.name in text.lower() else False
     
+    @staticmethod 
+    def action_time():
+        return datetime.datetime.now().strftime("%H:%M")        
+    
     @staticmethod
-    def text_to_speech(self, text):
+    def text_to_speech(text):
         print("AI ---> ", text)
         speaker = gTTS(text=text, lang="en", slow=False)
         speaker.save("response.mp3")
@@ -38,6 +42,20 @@ class ChatBot():
         
 if  __name__ == "__main__":
     bot = ChatBot(name="computer")
-    while(True):
+    
+    while True:
         bot.speech_to_text()
+        # wakeup
+        if bot.wake_up(bot.text) is True:
+            res = "Hello, my name is computer. What can I do for you?"
+        elif "time" in bot.text:
+            res = bot.action_time()
+        elif any(i in bot.text for i in ["thanks", "thanks"]):
+            res = np.random.choice(
+                ["You're Welcome!",
+                 "Anytime!",
+                 "No problem!",
+                 "Always here to help!"])
+                        
+        bot.text_to_speech(res)
 
